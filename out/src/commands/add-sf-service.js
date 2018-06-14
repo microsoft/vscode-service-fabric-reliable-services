864abdab06213c8ea6c281c831cc96f75720f336
Sudhanva Huruli <suhuruli@microsoft.com>
2018-06-14 09:36:16 -0700

    F# Logs logs *.log npm-debug.log* yarn-debug.log* yarn-error.log*
    pids
    *.pid
    *.seed
    *.pid.lock
    
    lib-cov
    
    coverage
    
    .nyc_output
    
    (http://gruntjs.com/creating-plugins#storing-task-files)
    .grunt
    
    bower_components
    
    .lock-wscript
    
    build/Release
    
    node_modules/
    jspm_packages/
    
    typings/
    
    .npm
    
    .eslintcache
    
    .node_repl_history
    
    *.tgz
    
    .yarn-integrity
    
    .env
    
    package-lock.json
    
    out
    *.vsix
    .vscode-tesix title and add icon

--- out/src/commands/add-sf-service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const yo_1 = require("../yo");
-function addSFService() {
-    yo_1.generatorProject(true);
-}
-exports.addSFService = addSFService;
-//# sourceMappingURL=add-sf-service.js.map
