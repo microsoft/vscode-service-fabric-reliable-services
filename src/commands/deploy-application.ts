9e476a481bae983aaeebd0e4e3eda5c92cb5ba7e
Vinay Bhat <vibha@microsoft.com>
2019-03-01 15:50:18 +0530

    Revent deploy

--- src/commands/deploy-application.ts
+++ src/commands/deploy-application.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import * as vars from './osdetector';
-import * as removeApplication from './remove-application';
 const exec = require('child_process').exec;
 
 var builScriptExtension;
@@ -47,42 +46,6 @@ async function installApplication(terminal:vscode.Terminal) {
         return;     
     }
     const relativeInstallPath = vscode.workspace.asRelativePath(uri[0]);
-    var cmd: string = './' + relativeInstallPath;
-    if (installScriptExtension === '.ps1') {
-        cmd = "try { " + cmd + " } catch { Write-Error 'Install Failed' }"
-    }
-    terminal.sendText(cmd);
+    terminal.sendText('./' + relativeInstallPath);
     terminal.show();
-    handleUncleanInstallation(terminal);
-}
-
-async function handleUncleanInstallation(terminal:vscode.Terminal) {
-    await checkCleanInstall(terminal).catch(_ => {
-        // installation failed
-        // uninstall
-        removeApplication.removeApplication();
-    });
-    cleanupLogFile();
-}
-
-async function checkCleanInstall(terminal:vscode.Terminal) {
-    terminal.sendText('$? > temp.log',true);
-    var fs = require('fs');
-    var outpath = vscode.workspace.workspaceFolders[0].uri.fsPath+'/temp.log';
-    var content;
-    return new Promise((resolve, reject) => {
-        setTimeout(function(){
-            content = fs.readFileSync(outpath, 'utf8');
-            if(content.includes('T') || content.includes('0'))
-                resolve(0);
-            else
-                reject(1);
-        },10000);
-    });
-}
-
-function cleanupLogFile() {
-    var fs = require('fs');
-    var outpath = vscode.workspace.workspaceFolders[0].uri.fsPath+'/temp.log';
-    fs.unlinkSync(outpath);
 }
