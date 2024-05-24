65ca0bdbe0d2d1a82521f2f79292247ece51eeea
Millie Olsen <millieolsen@microsoft.com>
2024-05-24 12:05:43 -0700

    Changing filepath

--- test/TestCSharpApplication/build2.cmd
+++ test/TestCSharpApplication/build2.cmd
@@ -1,2 +1,2 @@
-dotnet restore ../TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -s https://api.nuget.org/v3/index.json
-dotnet build ../TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -v normal
+dotnet restore %~dp0/../TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -s https://api.nuget.org/v3/index.json
+dotnet build %~dp0/../TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -v normal
