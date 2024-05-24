c00d84628bf38321e0f0e88e11fcb16f6aca11c5
Millie Olsen <millieolsen@microsoft.com>
2024-05-24 11:58:01 -0700

    Adding new build file

---
+++ test/TestCSharpApplication/build2.cmd
@@ -0,0 +1,2 @@
+dotnet restore ..\TestCSharpApplication\src\TestCSharpApplication\Service1\Service1.csproj -s https://api.nuget.org/v3/index.json
+dotnet build ..\TestCSharpApplication\src\TestCSharpApplication\Service1\Service1.csproj -v normal
