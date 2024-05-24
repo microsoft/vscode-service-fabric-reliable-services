0ee1c6cee23df632b4f2697f2f002a90299f6f2d
Millie Olsen <millieolsen@microsoft.com>
2024-05-24 12:34:24 -0700

    Enable CodeQL

---
+++ test/TestCSharpApplication/build_CodeQL.cmd
@@ -0,0 +1,2 @@
+dotnet restore test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -s https://api.nuget.org/v3/index.json
+dotnet build test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -v normal
