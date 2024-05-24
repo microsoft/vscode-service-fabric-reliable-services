d4179092a63a491578c5cc90e83d398d6fcf9a33
Millie Olsen <millieolsen@microsoft.com>
2024-05-24 12:17:35 -0700

    Code Cleanup and testing for loop

--- test/TestCSharpApplication/build_CodeQL.cmd
+++ test/TestCSharpApplication/build_CodeQL.cmd
@@ -1,2 +1,6 @@
 dotnet restore test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -s https://api.nuget.org/v3/index.json
 dotnet build test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -v normal
+
+for %%F in ("test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj") do cd %%~dpF
+dotnet publish -o test/TestCSharpApplication/TestCSharpApplication/Service1Pkg/Code
+cd %~dp0/..
